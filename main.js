348bcbdcf70be0cb863205da34da9fea0704934d
Laira Gabriela <127347225+lairafreiberger@users.noreply.github.com>
2024-04-12 10:42:04 -0300

    fch

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
+
+
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
 
@@ -19,8 +21,8 @@ const tempoObjetivo1 = new Date("2024-06-15T23:59:00");
 const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
 const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
 const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
-
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
+
 
 function atualizaCronometro(){
     for (let i=0; i<contadores.length;i++){
@@ -35,6 +37,7 @@ function comecaCronometro(){
 
 //comecaCronometro();
 
+
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
     let tempoFinal = tempoObjetivo - tempoAtual;
@@ -45,7 +48,6 @@ function calculaTempo(tempoObjetivo) {
     segundos %= 60;
     minutos %= 60;
     horas %= 24;
-    
     if (tempoFinal > 0){
         return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
     } else {
