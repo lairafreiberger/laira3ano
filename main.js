137bd9fcc6e7d6012e2b01207ff82079ef218571
Laira Gabriela <127347225+lairafreiberger@users.noreply.github.com>
2024-04-19 10:55:53 -0300

    rc

--- main.js
+++ main.js
@@ -26,10 +26,10 @@ const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
 
 function atualizaCronometro(){ 
-    document.getElementById("dias0").textContent = calculaTempo(tempos[1])[0];
-    document.getElementById("horas0").textContent = calculaTempo(tempos[1])[1];  
-    document.getElementById("min0").textContent = calculaTempo(tempos[1])[2];
-    document.getElementById("seg0").textContent = calculaTempo(tempos[1])[3];
+    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
+    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];  
+    document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
+    document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
     for (let i=0; i<contadores.length;i++){
         //contadores[i].textContent = calculaTempo(tempos[i]);   
     }
